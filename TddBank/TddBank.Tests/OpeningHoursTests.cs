f5f89922942c505e583a7c33bc212bc4736cfd33
André <AndreR@ppedv.de>
2023-03-13 15:25:41 +0100

    Test für Samstag

--- TddBank/TddBank.Tests/OpeningHoursTests.cs
+++ TddBank/TddBank.Tests/OpeningHoursTests.cs
@@ -11,6 +11,7 @@
         [InlineData(2023, 3, 13, 19, 00, false)] //mo
         [InlineData(2023, 3, 13, 13, 0, true)] //sa
         [InlineData(2023, 3, 18, 16, 0, false)] //sa
+        [InlineData(2023, 3, 18, 13, 0, true)] //sa
         [InlineData(2023, 3, 19, 20, 0, false)] //so
         public void OpeningHours_IsOpen(int y, int M, int d, int h, int m, bool result)
         {
