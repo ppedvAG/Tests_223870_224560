49faff1a736de0922280879e623aede986541c52
André <AndreR@ppedv.de>
2023-03-13 10:39:15 +0100

    Unitstest mit dem Calc

---
+++ Calculator/Calculator.Tests/CalcTests.cs
@@ -0,0 +1,52 @@
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class CalcTests
+    {
+        [TestMethod]
+        public void Sum_3_and_4_results_7()
+        {
+            //Arrange
+            Calc calc = new Calc();
+
+            //Act
+            var result = calc.Sum(3, 4);
+
+            //Assert
+            Assert.AreEqual(7, result);
+
+        }
+
+        [TestMethod]
+        public void Sum_n3_and_n4_results_n7()
+        {
+            //Arrange
+            Calc calc = new Calc();
+
+            //Act
+            var result = calc.Sum(-5, -4);
+
+            //Assert
+            Assert.AreEqual(-9, result);
+
+        }
+
+        [TestMethod]
+        [DataRow(0, 0, 0)]
+        [DataRow(1, 2, 3)]
+        [DataRow(5000, 2222, 7222)]
+        [DataRow(-5, -6, -11)]
+        [DataRow(5, -8, -3)]
+        public void Sum_with_datarows(int a, int b, int exp)
+        {
+            //Arrange
+            Calc calc = new Calc();
+
+            //Act
+            var result = calc.Sum(a, b);
+
+            //Assert
+            Assert.AreEqual(exp, result);
+        }
+    }
+}
