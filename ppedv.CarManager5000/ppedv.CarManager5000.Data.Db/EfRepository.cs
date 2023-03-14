7789b36c31d0ec7395c537ea0f153e23099a6cef
André <AndreR@ppedv.de>
2023-03-14 11:46:04 +0100

    EfRepo implementietrt

---
+++ ppedv.CarManager5000/ppedv.CarManager5000.Data.Db/EfRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using ppedv.CarManager5000.Model.Contracts;
+
+namespace ppedv.CarManager5000.Data.Db
+{
+    public class EfRepository : IRepository
+    {
+        CarManagerContext _context = new CarManagerContext();
+
+        public void Add<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : class
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById<T>(int id) where T : class
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public void SaveAll()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update<T>(T entity) where T : class
+        {
+            _context.Update(entity);
+        }
+    }
+}
