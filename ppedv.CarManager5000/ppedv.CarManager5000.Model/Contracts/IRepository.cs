7432e66e5798cec726d14343a4236b335f4643c7
André <AndreR@ppedv.de>
2023-03-14 11:42:07 +0100

    Irepo definiert

---
+++ ppedv.CarManager5000/ppedv.CarManager5000.Model/Contracts/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace ppedv.CarManager5000.Model.Contracts
+{
+    public interface IRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+
+        T GetById<T>(int id) where T : class;
+        IEnumerable<T> GetAll<T>() where T : class;
+
+        void SaveAll();
+    }
+}
