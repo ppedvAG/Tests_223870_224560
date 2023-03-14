45eb3ef6385b0a4a1fb94c2c13d2584c32f86738
André <AndreR@ppedv.de>
2023-03-14 10:30:02 +0100

    read Test

--- ppedv.CarManager5000/ppedv.CarManager5000.Data.Db.Tests/CarManagerContextTests.cs
+++ ppedv.CarManager5000/ppedv.CarManager5000.Data.Db.Tests/CarManagerContextTests.cs
@@ -1,3 +1,5 @@
+using ppedv.CarManager5000.Model;
+
 namespace ppedv.CarManager5000.Data.Db.Tests
 {
     public class CarManagerContextTests
@@ -12,5 +14,42 @@ namespace ppedv.CarManager5000.Data.Db.Tests
 
             Assert.True(result);
         }
+
+        [Fact]
+        public void Can_insert_Car()
+        {
+            var con = new CarManagerContext();
+            con.Database.EnsureCreated();
+            var car = new Car() { Model = "XYZ", Color = "gelb", KW = 85, Weight = 874.3 };
+
+            con.Add(car);
+            int count = con.SaveChanges();
+
+            Assert.Equal(1, count);
+        }
+
+        [Fact]
+        public void Can_read_Car()
+        {
+            var car = new Car() { Model = "XYZ", Color = "gelb", KW = 85, Weight = 874.3 };
+            using (var con = new CarManagerContext())
+            {
+                con.Database.EnsureCreated();
+                con.Add(car);
+                con.SaveChanges();
+            }
+
+            using (var con = new CarManagerContext())
+            {
+                var loadedCar = con.Cars.Find(car.Id);
+                Assert.NotNull(loadedCar);
+                Assert.Equal(car.Model, loadedCar.Model);
+                Assert.Equal(car.Color, loadedCar.Color);
+                Assert.Equal(car.KW, loadedCar.KW);
+                Assert.Equal(car.Weight, loadedCar.Weight);
+            }
+        }
+
+
     }
 }
