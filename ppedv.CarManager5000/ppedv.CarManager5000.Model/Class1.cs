87b436efac388cd57f3094733218c6199d27edc6
André <AndreR@ppedv.de>
2023-03-14 09:48:46 +0100

    Model für CarManager

---
+++ ppedv.CarManager5000/ppedv.CarManager5000.Model/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace ppedv.CarManager5000.Model
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Manufacturer { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public string? Color { get; set; }
+        public int KW { get; set; }
+        public DateTime BuildDate { get; set; }
+        public double Weight { get; set; }
+    }
+}
